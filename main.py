e8c068a257cfe811bf31efbdf631fc238986cfff
Minatozaki <148259896+Minatozaki-A@users.noreply.github.com>
2026-04-16 17:01:03 -0600

    prueba de python-telegram-bot

---
+++ main.py
@@ -0,0 +1,25 @@
+import logging
+from services.memory_info import get_virtual_memory_info, get_swap_memory_info
+import pathlib
+import os
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+async def memory(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Memory:\n'
+                                    f'{get_virtual_memory_info()}')
+
+
+app = ApplicationBuilder().token("8675810082:AAERru3Npm6_yTpcadeMdnAazLRyN66icQ8").build()
+
+app.add_handler(CommandHandler("hello", hello))
+app.add_handler(CommandHandler("memory",  memory))
+app.run_polling()
+
+
+
+
