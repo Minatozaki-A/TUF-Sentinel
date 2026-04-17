0f4a03926be2269c955a522170b485051df531c9
Minatozaki <148259896+Minatozaki-A@users.noreply.github.com>
2026-04-17 17:55:43 -0600

    se creo un filtro de usuarios

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import logging
 from services.monitor import get_virtual_memory, get_swap_memory
-import pathlib
-import os
-
+from dotenv import load_dotenv
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, filters, MessageHandler
+import os
+from utils.helpers import format_cpu_status, get_formatted_uptime
+
+load_dotenv()
+token = os.getenv("TOKEN")
+id_user = int(os.getenv("USER_ID"))
+
+only_me = filters.User(user_id=id_user)
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
-async def memory(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def memory_info(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     vm = get_virtual_memory()
     await update.message.reply_text(
         f'Memory:\n'
@@ -25,11 +31,16 @@ async def memory(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         f'Shared: {vm.shared}\n'
     )
 
+async def cpu_info(update: Update, context: ContextTypes.DEFAULT_TYPE)-> None:
+    pass
 
-app = ApplicationBuilder().token("Token").build()
+app = ApplicationBuilder().token(token).build()
 
-app.add_handler(CommandHandler("hello", hello))
-app.add_handler(CommandHandler("memory",  memory))
+app.add_handler(MessageHandler(only_me & filters.TEXT & ~filters.COMMAND, hello))
+app.add_handler(CommandHandler("hello", hello, filters=only_me))
+app.add_handler(CommandHandler("memory", memory_info, filters=only_me))
+app.add_handler(CommandHandler("cpu", cpu_info, filters=only_me))
+
 app.run_polling()
 
 
