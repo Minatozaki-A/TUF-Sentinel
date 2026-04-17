8c9d5bb51dbda5533f5d3d7310ce827c8eacf29c
Minatozaki <148259896+Minatozaki-A@users.noreply.github.com>
2026-04-17 15:10:56 -0600

    se agregaron comentarios sobre que retorna cada funcion

--- services/monitor.py
+++ services/monitor.py
@@ -13,27 +13,28 @@ from psutil import (
 def get_cpu_times():
     return cpu_times()
 
-def get_cpu_percent():
+def get_cpu_percent():# Return a float representing the current system-wide CPU utilization as a percentage. 
     return cpu_percent()
 
-def get_cpu_times_percent():
+def get_cpu_times_percent():#Same as cpu_percent() but provides utilization percentages for each specific CPU time as is returned by psutil.cpu_times(percpu=True)
     return cpu_times_percent()
 
-def get_cpu_count():
+def get_cpu_count():# Return the number of logical CPUs in the system (similar to os.cpu_count) or None
     return cpu_count()
 
-def get_cpu_stats():
+def get_cpu_stats():# Return various CPU statistics as a named tuple
     return cpu_stats()
 
-def get_cpu_freq():
+def get_cpu_freq():# Return CPU frequency 
     return cpu_freq()
 
 
 # memory_info.py
-def get_virtual_memory():
+# RAM
+def get_virtual_memory():# Return statistics about system memory usage 
     return virtual_memory()
 
-def get_swap_memory():
+def get_swap_memory():# Return system swap memory statistics
     return swap_memory()
 
 
@@ -42,13 +43,13 @@ def get_swap_memory():
 
 
 # disks_info.py
-def get_disk_partitions():
+def get_disk_partitions():# Return all mounted disk partitions as a list of named tuples including device, mount point and filesystem type, similarly to “df” command on UNIX. If all parameter is False it tries to distinguish and return physical devices only
     return disk_partitions()
 
-def get_disk_usage(path="/"):
+def get_disk_usage(path="/"):# Return system-wide disk I/O statistics 
     return disk_usage(path)
 
-def get_disk_io_counters():
+def get_disk_io_counters():# Return system-wide disk I/O statistics 
     return disk_io_counters()
 
 
@@ -61,33 +62,33 @@ def get_disk_io_counters():
 
 
 # sensors_info.py
-def get_sensors_temperatures():
+def get_sensors_temperatures():# Return hardware temperatures.
     return sensors_temperatures()
 
-def get_sensors_fans():
+def get_sensors_fans():# Return hardware fans speed
     return sensors_fans()
 
-def get_sensors_battery():
-    return sensors_battery()
+def get_sensors_battery():# Return hardware fans speed. 
+    return sensors_battery() # power_plugged: True if the AC power cable is connected, False if not or None
 
 
 # network_info.py
-def get_net_io_counters():
+def get_net_io_counters(): # Return system-wide network I/O statistics
     return net_io_counters()
 
-def get_net_connections():
+def get_net_connections():# Return system-wide socket connections as a list of named tuples
     return net_connections()
 
-def get_net_if_addrs():
+def get_net_if_addrs():# Return the addresses associated to each NIC
     return net_if_addrs()
 
-def get_net_if_stats():
+def get_net_if_stats():# Return information about each NIC (network interface card) installed on the system
     return net_if_stats()
 
 
 # users_info.py
-def get_users():
+def get_users():# Return users currently connected
     return users()
 
-def get_boot_time():
+def get_boot_time():# Return the system boot time expressed in seconds since the epoch (seconds since January 1, 1970, at midnight UTC)
     return boot_time()
