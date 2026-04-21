f7bd123211eb7069392e1ad342c7fe06d5008ee9
Minatozaki <148259896+Minatozaki-A@users.noreply.github.com>
2026-04-20 18:08:31 -0600

    prueba a api nvd

--- services/cve_notifier.py
+++ services/cve_notifier.py
@@ -1,3 +1,18 @@
 # Consulta de la API de NVD
 import requests
+from dotenv import load_dotenv
+import logging
+import os
 
+load_dotenv()
+api_key = os.getenv("API_KEY")
+
+URL = "https://services.nvd.nist.gov/rest/json/cpes/2.0?cpeNameId="+api_key
+
+
+response = requests.get(URL)
+if response.status_code == 200:
+    logging.info("CVEs notificadas")
+    print('Data:', response.json())
+else:
+    print('Error en la solicitud, detalles:', response.text)
