2af9fb9ec9ea1b33180eb09308567d545593de31
Minatozaki <148259896+Minatozaki-A@users.noreply.github.com>
2026-05-12 16:21:28 -0600

    refactor(config): centralize environment variable access

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,23 @@
 # Tokens y rutas de carpetas
 import os
 import dotenv
+
 dotenv.load_dotenv()
+
+def get_user_id():
+    try:
+        return int(os.getenv("USER_ID"))
+    except KeyError:
+        return None
+
+def get_token():
+    try:
+        return os.getenv("TOKEN")
+    except KeyError:
+        return None
+
+def get_label_mount():
+    try:
+        return os.getenv("LABEL_MOUNT")
+    except KeyError:
+        return None
