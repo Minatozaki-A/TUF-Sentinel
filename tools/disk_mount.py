510f6f1c8d094e7c64f3b641091059da82372804
Minatozaki <148259896+Minatozaki-A@users.noreply.github.com>
2026-06-04 15:15:16 -0600

    refactor(storage): use env LABEL_MOUNT and pathlib for mount detection

--- tools/disk_mount.py
+++ tools/disk_mount.py
@@ -1,16 +1,22 @@
 import logging
+from pathlib import Path
+
 from psutil import disk_partitions
 
+from utils.config import collect_info_by_env
 
-def find_mount_point_by_label(label_mount: str = None)-> str | None:
-
+def find_mount_point_by_label()-> str | None:
+    label_mount = collect_info_by_env("LABEL_MOUNT")
+    parent_path = Path("/mnt")
     if not label_mount:
         raise ValueError("label_mount is empty")
 
     for part in disk_partitions():
-        mount_point = part.mountpoint.rstrip('/')
-        if mount_point.endswith(label_mount):
+        mount_point = Path(part.mountpoint)
+
+        if mount_point.parent == parent_path and mount_point.name == label_mount:
             logging.info("SSD found - device: %s | mount point: %s | filesystem: %s",
                         part.device, part.mountpoint, part.fstype)
             return part.mountpoint
     return None
+
